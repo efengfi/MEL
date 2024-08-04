3bd95585c418b9663b02a39891f9a74f3a47f5a5
Ethan Feng <efeng@ficonsulting.com>
2024-08-04 13:33:59 -0400

    histogram

--- src/app/histogram/histogram.component.ts
+++ src/app/histogram/histogram.component.ts
@@ -1,10 +1,164 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener, AfterViewInit } from '@angular/core';
+import * as d3 from 'd3';
+
+interface EventData {
+  date: string;
+  event: string;
+  frequency: number;
+}
 
 @Component({
   selector: 'app-histogram',
   templateUrl: './histogram.component.html',
   styleUrls: ['./histogram.component.css']
 })
-export class HistogramComponent {
+export class HistogramComponent implements OnInit, AfterViewInit {
+  private data: EventData[] = [
+    { date: '2023-07-01', event: 'Event A', frequency: 10 },
+    { date: '2023-07-02', event: 'Event B', frequency: 20 },
+    { date: '2023-07-03', event: 'Event C', frequency: 15 },
+    { date: '2023-07-04', event: 'Event A', frequency: 30 },
+    { date: '2023-07-05', event: 'Event B', frequency: 25 },
+    { date: '2023-07-06', event: 'Event C', frequency: 5 },
+    { date: '2023-07-07', event: 'Event A', frequency: 40 },
+    { date: '2023-07-08', event: 'Event B', frequency: 35 },
+    { date: '2023-07-09', event: 'Event C', frequency: 10 }
+  ];
 
+  private svg: any;
+  private margin = { top: 50, right: 150, bottom: 50, left: 50 };
+  private width: number = 0;
+  private height: number = 0;
+
+  private colors = d3.scaleOrdinal()
+    .domain(['Event A', 'Event B', 'Event C'])
+    .range(['#1f77b4', '#ff7f0e', '#2ca02c']);
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    this.updateDimensions();
+  }
+
+  ngAfterViewInit(): void {
+    this.createSvg();
+    this.drawBars(this.data);
+    this.addLegend();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.updateDimensions();
+    this.redraw();
+  }
+
+  private updateDimensions(): void {
+    const element = this.el.nativeElement.querySelector('.histogram-container');
+    const containerWidth = element.clientWidth;
+    const containerHeight = element.clientHeight;
+
+    this.width = containerWidth - this.margin.left - this.margin.right;
+    this.height = containerHeight - this.margin.top - this.margin.bottom;
+  }
+
+  private createSvg(): void {
+    const element = this.el.nativeElement.querySelector('.histogram-container');
+    const containerWidth = element.clientWidth;
+    const containerHeight = element.clientHeight;
+
+    this.svg = d3.select(element).append('svg')
+      .attr('width', containerWidth)
+      .attr('height', containerHeight)
+      .append('g')
+      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
+
+    this.svg.append('rect')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .attr('fill', 'red');
+  }
+
+  private drawGridLines(y: d3.ScaleLinear<number, number, never>): void {
+    this.svg.append('g')
+      .attr('class', 'grid')
+      .call(d3.axisLeft(y)
+        .tickSize(-this.width)
+        .tickFormat(() => '')
+      )
+      .selectAll('.tick line')
+      .attr('stroke', 'blue');
+  }
+
+  private drawBars(data: EventData[]): void {
+    this.svg.selectAll('*').remove();
+
+    this.svg.append('rect')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .attr('fill', 'red');
+
+    const x = d3.scaleBand()
+      .range([0, this.width])
+      .domain(data.map(d => d.date))
+      .padding(0.2);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.frequency)!])
+      .range([this.height, 0]);
+
+    this.drawGridLines(y);
+
+    this.svg.append('g')
+      .attr('transform', `translate(0,${this.height})`)
+      .call(d3.axisBottom(x).tickSize(0))
+      .selectAll('.domain, .tick line')
+      .attr('stroke', 'none');
+
+    this.svg.append('g')
+      .call(d3.axisLeft(y).ticks(5).tickSize(0))
+      .selectAll('.domain, .tick line')
+      .attr('stroke', 'none');
+
+    this.svg.selectAll('rect.bar')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d: EventData) => x(d.date)!)
+      .attr('y', (d: EventData) => y(d.frequency))
+      .attr('width', x.bandwidth())
+      .attr('height', (d: EventData) => this.height - y(d.frequency))
+      .attr('fill', (d: EventData) => this.colors(d.event));
+  }
+
+  private addLegend(): void {
+    const events = Array.from(new Set(this.data.map(d => d.event)));
+
+    const legend = this.svg.selectAll('.legend')
+      .data(events)
+      .enter().append('g')
+      .attr('class', 'legend')
+      .attr('transform', (_: string, i: number) => `translate(0,${i * 20})`);
+
+    legend.append('rect')
+      .attr('x', this.width + 20)
+      .attr('y', 0)
+      .attr('width', 18)
+      .attr('height', 18)
+      .style('fill', (d: string) => this.colors(d));
+
+    legend.append('text')
+      .attr('x', this.width + 40)
+      .attr('y', 9)
+      .attr('dy', '.35em')
+      .style('text-anchor', 'start')
+      .text((d: string) => d);
+  }
+
+  private redraw(): void {
+    d3.select(this.el.nativeElement).select('svg').remove();
+    this.createSvg();
+    this.drawBars(this.data);
+    this.addLegend();
+  }
 }
